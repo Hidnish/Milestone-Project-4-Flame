5b7cd5155231ac24c7c55702f68f8c1425ee5fc0
Hidnish <hideonishimura@live.it>
2021-12-18 10:06:38 +0000

    customize admin

--- home/admin.py
+++ home/admin.py
@@ -4,6 +4,31 @@ from .models import Product, Category, Brand
 # Register your models here.
 
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Brand)
+class ProductAdmin(admin.ModelAdmin):
+    list_dispaly = (
+        'code',
+        'name',
+        'description',
+        'price',
+        'image',
+    )
+    # has to be a tuple to order based on multiple columns 
+    ordering = ('code', )
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_dispaly = (
+        'name',
+        'friendly_name',
+    )
+
+class BrandAdmin(admin.ModelAdmin):
+    list_dispaly = (
+        'name',
+        'friendly_name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Brand, BrandAdmin)
+
