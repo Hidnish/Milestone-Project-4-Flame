5cbcac22e0947cf4a918bfffb272f681117a1b41
Hidnish <hideonishimura@live.it>
2021-12-23 12:06:53 +0000

    Deploy project - 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: flame.wsgi:application
--- flame/settings.py
+++ flame/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-nt3od#ecfiw0mg#1*!59jeako#x1e5$g@bhz^1@z+c8c5q+1ol
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['milestone-project-4-flame.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -110,12 +110,17 @@ WSGI_APPLICATION = 'flame.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL')),
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
