6602212b9e784c66d7c57ae7daf08be00a8f69ef
Hidnish <hideonishimura@live.it>
2021-12-30 16:43:16 +0000

    Add toasts

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages 
+from products.models import Product 
 
 # Create your views here.
 
@@ -12,6 +14,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add quantity of the selected product to the cart """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     # create an empty dictionary if cart doesn't exist
@@ -21,6 +24,7 @@ def add_to_cart(request, item_id):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
+        messages.error(request, f'Added {product.name} to your bag')
 
     request.session['cart'] = cart
 
--- flame/settings.py
+++ flame/settings.py
@@ -88,6 +88,9 @@ TEMPLATES = [
     },
 ]
 
+# For toasts 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
