a894af774982bd285b297b12bb1789dc5265e947
Hidnish <hideonishimura@live.it>
2022-01-17 12:26:47 +0000

    Move base js to separate folder

---
+++ static/js/base.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+    // Sidenav for medium to xs screen sizes 
+    $('.navbar-toggler, .overlay').on('click', function () {
+        $('.mobileMenu, .overlay').toggleClass('open mobileMenuOpen');
+    });
+
+    // Slide effect from: https://newbedev.com/adding-a-slide-effect-to-bootstrap-dropdown
+    $('.dropdown').on('show.bs.dropdown', function () {
+        if ($(window).width() <= 992) {
+            $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+        } else {
+            $(this).find('.dropdown-menu').first().stop(false, false).fadeIn('fast');
+        }
+    });
+    // Add slideUp animation to Bootstrap dropdown when collapsing.
+    $('.dropdown').on('hide.bs.dropdown', function () {
+        if ($(window).width() <= 992) {
+            $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+        } else {
+            $(this).find('.dropdown-menu').first().stop(true, true).fadeOut('fast');
+        }
+    })
+
+
+    $(document).scroll(function () {
+        if ($(this).scrollTop() >= $(".navbar").height()) {
+            $(".navbar").addClass('scrolled');
+            $(".navbar").removeClass('navbar-bg');
+        } else {
+            $(".navbar").removeClass('scrolled');
+            $(".navbar").addClass('navbar-bg')
+        }
+    });
+
+    // loader 
+    $(window).ready(function () {
+        $(".loader").fadeOut("slow")
+        $(".loader2").fadeOut("fast");
+    });
+});
