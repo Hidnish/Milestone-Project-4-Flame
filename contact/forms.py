beba4392a7fadd67c1e5e712b68d2413b349efaf
Hidnish <hideonishimura@live.it>
2022-01-21 16:03:20 +0000

    Remove form labels and add placeholders

--- contact/forms.py
+++ contact/forms.py
@@ -18,5 +18,15 @@ class MessageForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        placeholders = {
+            'email': 'Email Address',
+            'subject': 'Subject',
+            'message': 'Message',
+        }
+
         for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'radius-10'
+            self.fields[field].label = False
