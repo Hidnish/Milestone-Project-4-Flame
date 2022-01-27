e6980a754c4e4f38c26be8850627e937d3d6b45d
Hidnish <hideonishimura@live.it>
2022-01-27 17:12:05 +0000

    Limit the purchase amount for checkout to €9999

--- checkout/views.py
+++ checkout/views.py
@@ -36,101 +36,106 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    if request.method == 'POST':
-        cart = request.session.get('cart', {})
+    if cart_contents(request)['grand_total'] <= 9999:
+        if request.method == 'POST':
+            cart = request.session.get('cart', {})
 
-        form_data = {
-            'full_name': request.POST['full_name'],
-            'email': request.POST['email'],
-            'phone_number': request.POST['phone_number'],
-            'country': request.POST['country'],
-            'postcode': request.POST['postcode'],
-            'town_or_city': request.POST['town_or_city'],
-            'street_address1': request.POST['street_address1'],
-            'street_address2': request.POST['street_address2'],
-            'county': request.POST['county'],
-        }
+            form_data = {
+                'full_name': request.POST['full_name'],
+                'email': request.POST['email'],
+                'phone_number': request.POST['phone_number'],
+                'country': request.POST['country'],
+                'postcode': request.POST['postcode'],
+                'town_or_city': request.POST['town_or_city'],
+                'street_address1': request.POST['street_address1'],
+                'street_address2': request.POST['street_address2'],
+                'county': request.POST['county'],
+            }
 
-        order_form = OrderForm(form_data)
-        if order_form.is_valid():
-            # Prevents multiple save events in the database
-            order = order_form.save(commit=False)
-            pid = request.POST.get('client_secret').split('_secret')[0]
-            order.stripe_pid = pid
-            order.original_cart = json.dumps(cart)
-            order.save()
-            for item_id, item_data in cart.items():
+            order_form = OrderForm(form_data)
+            if order_form.is_valid():
+                # Prevents multiple save events in the database
+                order = order_form.save(commit=False)
+                pid = request.POST.get('client_secret').split('_secret')[0]
+                order.stripe_pid = pid
+                order.original_cart = json.dumps(cart)
+                order.save()
+                for item_id, item_data in cart.items():
+                    try:
+                        product = Product.objects.get(pk=item_id)
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    except Product.DoesNotExist:
+                        messages.error(
+                            request, ('One of the products in the cart was not found in the database. '
+                                    'Please call us for assistance'))
+                        order.delete()
+                        return redirect(reverse('view_cart'))
+
+                request.session['save_info'] = 'save-info' in request.POST
+                return redirect(
+                    reverse(
+                        'checkout_success',
+                        args=[
+                            order.order_number]))
+            else:
+                messages.error(request, 'There was an error with your form. \
+                    Please double check your information.')
+        else:
+            cart = request.session.get('cart', {})
+
+            if not cart:
+                messages.error(request, 'The cart is empty at the moment')
+                return redirect(reverse('products'))
+
+            current_cart = cart_contents(request)
+            total = current_cart['grand_total']
+            stripe_total = round(total * 100)
+            stripe.api_key = stripe_secret_key
+            intent = stripe.PaymentIntent.create(
+                amount=stripe_total,
+                currency=settings.STRIPE_CURRENCY,
+            )
+
+            if request.user.is_authenticated:
                 try:
-                    product = Product.objects.get(pk=item_id)
-                    order_line_item = OrderLineItem(
-                        order=order,
-                        product=product,
-                        quantity=item_data,
-                    )
-                    order_line_item.save()
-                except Product.DoesNotExist:
-                    messages.error(
-                        request, ('One of the products in the cart was not found in the database. '
-                                  'Please call us for assistance'))
-                    order.delete()
-                    return redirect(reverse('view_cart'))
-
-            request.session['save_info'] = 'save-info' in request.POST
-            return redirect(
-                reverse(
-                    'checkout_success',
-                    args=[
-                        order.order_number]))
-        else:
-            messages.error(request, 'There was an error with your form. \
-                Please double check your information.')
-    else:
-        cart = request.session.get('cart', {})
-
-        if not cart:
-            messages.error(request, 'The cart is empty at the moment')
-            return redirect(reverse('products'))
-
-        current_cart = cart_contents(request)
-        total = current_cart['grand_total']
-        stripe_total = round(total * 100)
-        stripe.api_key = stripe_secret_key
-        intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
-        )
-
-        if request.user.is_authenticated:
-            try:
-                profile = UserProfile.objects.get(user=request.user)
-                order_form = OrderForm(initial={
-                    'full_name': profile.full_name,
-                    'email': profile.user.email,
-                    'phone_number': profile.default_phone_number,
-                    'country': profile.default_country,
-                    'postcode': profile.default_postcode,
-                    'town_or_city': profile.default_town_or_city,
-                    'street_address1': profile.default_street_address1,
-                    'street_address2': profile.default_street_address2,
-                    'county': profile.default_county,
-                })
-            except UserProfile.DoesNotExist:
+                    profile = UserProfile.objects.get(user=request.user)
+                    order_form = OrderForm(initial={
+                        'full_name': profile.full_name,
+                        'email': profile.user.email,
+                        'phone_number': profile.default_phone_number,
+                        'country': profile.default_country,
+                        'postcode': profile.default_postcode,
+                        'town_or_city': profile.default_town_or_city,
+                        'street_address1': profile.default_street_address1,
+                        'street_address2': profile.default_street_address2,
+                        'county': profile.default_county,
+                    })
+                except UserProfile.DoesNotExist:
+                    order_form = OrderForm()
+            else:
                 order_form = OrderForm()
-        else:
-            order_form = OrderForm()
 
-        if not stripe_public_key:
-            messages.warning(request, 'Stripe public key is missing. \
-                Did you forget to set it in your environment?')
+            if not stripe_public_key:
+                messages.warning(request, 'Stripe public key is missing. \
+                    Did you forget to set it in your environment?')
 
-        template = 'checkout/checkout.html'
-        context = {
-            'order_form': order_form,
-            'stripe_public_key': stripe_public_key,
-            'client_secret': intent.client_secret,
-        }
+            template = 'checkout/checkout.html'
+            context = {
+                'order_form': order_form,
+                'stripe_public_key': stripe_public_key,
+                'client_secret': intent.client_secret,
+            }
 
-        return render(request, template, context)
+            return render(request, template, context)
+    else:
+        messages.error(request, 'The amount of your purchase can not go above \
+                                 €9999. Plase adjust the content of you cart.')
+        return redirect(reverse('view_cart'))
 
 
 def checkout_success(request, order_number):
