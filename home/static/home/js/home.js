ff7eb628253582bff60103b1a5bbe68723cd3373
Hidnish <hideonishimura@live.it>
2022-01-27 15:47:23 +0000

    Limit the max quantity of a product to 10

---
+++ home/static/home/js/home.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    new Glider(document.querySelector('.glider'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        draggable: true,
+        dots: '.dots',
+        arrows: {
+            prev: '.glider-prev',
+            next: '.glider-next'
+        },
+        responsive: [{
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        }, {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        }]
+    });
+})
