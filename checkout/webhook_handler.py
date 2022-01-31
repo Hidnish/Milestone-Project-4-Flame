11c89dcc8a197d3000f43a56d6414b34e674264d
Hidnish <hideonishimura@live.it>
2022-01-31 09:17:24 +0000

    Remove unnecessary commas

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -67,13 +67,13 @@ class StripeWH_Handler:
         if username != 'AnonymousUser':
             if save_info:
                 profile = UserProfile.objects.get(user__username=username)
-                profile.default_phone_number = shipping_details.phone,
-                profile.default_country = shipping_details.address.country,
-                profile.default_postcode = shipping_details.address.postal_code,
-                profile.default_town_or_city = shipping_details.address.city,
-                profile.default_street_address1 = shipping_details.address.line1,
-                profile.default_street_address2 = shipping_details.address.line2,
-                profile.default_county = shipping_details.address.state,
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
                 profile.save()
 
         order_exists = False
